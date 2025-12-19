35942aa07006b543c1a960aaf3a6411343c4fe5f
Dieu Hoang <dieu300504@gmail.com>
2025-12-19 17:37:35 +0700

    Cập nhật BE

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
+require("dotenv").config({ quiet: true });
 const express = require("express"); 
+const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express(); 
-
+    app.use(cors()); 
+    app.use(express.json()); 
+// Connect to MongoDB   
+    mongoose.connect(process.env.MONGO_URI)
+        .then(() => console.log("Connected to MongoDB"))
+        .catch((err) => console.error("MongoDB connection error:", err));
     app.get("/", async (req, res) => { 
          res.status(200).send("Xin chào bạn"); 
 }); 
 // Start server 
-    app.listen(3000, () => { 
+    app.listen(process.env.PORT || 3000, () => { 
         console.log("Server running on http://localhost:3000"); 
 }); 
